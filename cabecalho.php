34e7647cf9ab519cce29a1979a5c11d56bc4a6f3
Sasmuka06 <samuelssf881@gmail.com>
2025-02-11 14:33:11 -0300

    Adcionei um cabecalho e um link para a pagina mostruairo no cabecalho

--- cabecalho.php
+++ cabecalho.php
@@ -8,3 +8,33 @@
 </head>
 <body>
 
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+
+    <a class="navbar-brand" href="#">Blanc Semijoias</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="mostruario.php">Mostruario</a>
+        </li>
+        <li class="nav-item dropdown">
+          
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
+<div class="container"> 
